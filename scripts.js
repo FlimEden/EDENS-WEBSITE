aeac70dcbda8771890b9607ca4178c97da990d25
Eden Li <edenli@stu-c02p31nug5rp-3.local>
2017-12-27 22:43:00 +0800

    updated from airport

--- scripts.js
+++ scripts.js
@@ -1,24 +1,10 @@
-/**
- * This was built using the scrollie jQuery Plugin
- * https://github.com/Funsella/jquery-scrollie
- */
-
-
-$( window ).ready(function() {
-  
-    var wHeight = $(window).height();
-
-    $('.slide')
-      .height(wHeight)
-      .scrollie({
-        scrollOffset : -50,
-        scrollingInView : function(elem) {
-                   
-          var bgColor = elem.data('background');
-          
-          $('body').css('background-color', bgColor);
-          
-        }
-      });
-
-  });
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+         $(‘body’).addClass(‘changeColor’)
+      }
+      if ($(this).scrollTop() < 50) {
+         $(‘body’).removeClass(‘changeColor’)
+      }
+   });
+});
